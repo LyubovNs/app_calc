5536eaed562396c292851cf784b50e2eab60b297
lyuba <>
2022-05-19 00:19:10 +0700

    add new .py for telega_bot

--- telegaCalc/start_bot.py
+++ telegaCalc/start_bot.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
 import logging
 import venv
 
@@ -42,9 +43,6 @@ def echo(update, context):
     elif ("off" == command):
         context.user_data[echo] = False
         update.message.reply_text("Repeater Stopped")
-
-
-
 
 start_bot()
 
